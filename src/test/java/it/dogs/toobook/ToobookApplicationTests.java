a9561e50cf913c9133d77e46df136b277074a171
federicomosca <federicomosca01@outlook.it>
2025-09-07 18:46:22 +0200

    uploading basic spring app structure

---
+++ src/test/java/it/dogs/toobook/ToobookApplicationTests.java
@@ -0,0 +1,13 @@
+package it.dogs.toobook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToobookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
