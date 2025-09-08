659a9e057f6098d90090ad4d30ce0a184504f82f
Federico Mosca <f.mosca@xrmhub.it>
2025-09-08 17:40:25 +0200

    add a pair of endpoints for book

--- src/main/java/it/dogs/toobook/controller/BookController.java
+++ src/main/java/it/dogs/toobook/controller/BookController.java
@@ -2,7 +2,10 @@ package it.dogs.toobook.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +16,7 @@ import it.dogs.toobook.service.BookService;
 @RestController
 @RequestMapping("/books")
 public class BookController {
-    
+
     private final BookService bookService;
 
     public BookController(BookService bookService) {
@@ -24,5 +27,17 @@ public class BookController {
     public ResponseEntity<Book> addBook(@RequestBody Book book) {
         Book addedBook = bookService.addBook(book);
         return ResponseEntity.status(HttpStatus.CREATED).body(addedBook);
+    }
+
+    @PutMapping
+    public ResponseEntity<Book> updateBookDetails(@RequestBody Book book, Long id) {
+        Book updatedBook = bookService.updateBookDetails(id, book);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(updatedBook);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> removeBook(@PathVariable Long id) {
+        bookService.removeBook(id);
+        return ResponseEntity.status(HttpStatus.OK).body(null);
     }
 }
